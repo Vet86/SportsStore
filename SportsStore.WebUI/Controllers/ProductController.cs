90fa349af76e8565c4260d9399070ab20f66b8a5
Vet86 <vitalyev_aleksey@mail.ru>
2019-07-18 23:03:35 +0300

    Add Moc repo

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET: Product
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
